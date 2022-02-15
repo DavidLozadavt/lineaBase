7b6728d1ab1fc175f28882f445d8b0d8aab5db66
Omar David Sanchez Arroyo <odsanchez34@gmail.com>
2022-02-15 15:11:12 -0500

    spa

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// SPA
+Route::get(
+    '/{any?}',
+    [
+        App\Http\Controllers\WebController::class,
+        'index'
+    ]
+);
