fccb3e39dfc5d6de7915c5e2c9296e2cf61f0ab2
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-24 12:08:35 -0500

    Fix code of TipoTransaccionController

--- app/Http/Controllers/gestion_tipotransaccion/TipoTransaccionController.php
+++ app/Http/Controllers/gestion_tipotransaccion/TipoTransaccionController.php
@@ -4,78 +4,92 @@ namespace App\Http\Controllers\gestion_tipotransaccion;
 
 use App\Http\Controllers\Controller;
 use App\Models\TipoTransaccion;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TipoTransaccionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $tipoTransacciones = TipoTransaccion::all();
+  /**
+   * Get all data tipo transaccion
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function index(): JsonResponse
+  {
+    $tipoTransacciones = TipoTransaccion::all();
 
-        return response()->json($tipoTransacciones);
+    return response()->json($tipoTransacciones);
+  }
+
+  /**
+   * Store data of tipo transaccion
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function store(Request $request): JsonResponse
+  {
+    $data = $request->all();
+    $tipoTransaccion = new TipoTransaccion($data);
+    $tipoTransaccion->save();
+
+    return response()->json($tipoTransaccion, 201);
+  }
+
+  /**
+   * Get data tipo transaccion by id
+   *
+   * @param  int $id
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function show(int $id): JsonResponse
+  {
+    $tipoTransaccion = TipoTransaccion::find($id);
+
+    if(!$tipoTransaccion)
+    {
+      return response()->json(['error' => 'Tipo transaccion not found'], 404);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        $tipoTransaccion = new TipoTransaccion($data);
-        $tipoTransaccion->save();
+    return response()->json($tipoTransaccion);
+  }
 
-        return response()->json($tipoTransaccion, 201);
+  /**
+   * Update tipo transaccion by id
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int $id
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function update(Request $request, int $id): JsonResponse
+  {
+    try {
+      $data = $request->all();
+      $tipoTransaccion = TipoTransaccion::findOrFail($id);
+      $tipoTransaccion->fill($data);
+      $tipoTransaccion->save();
+    } catch (\Exception $e) {
+      return response()->json(['Tipo transaccion no found or ' . $e]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(int $id)
-    {
-        $tipoTransaccion = TipoTransaccion::find($id);
+    return response()->json($tipoTransaccion);
+  }
 
-        return response()->json($tipoTransaccion);
+  /**
+   * Remove Tipo transaccion by id
+   *
+   * @param  int $id
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function destroy(int $id): JsonResponse
+  {
+    try {
+      $tipoTransaccion = TipoTransaccion::findOrFail($id);
+      $tipoTransaccion->delete();
+    } catch (\Exception $e) {
+      return response()->json(['Tipo transaccion no found or ' . $e]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, int $id)
-    {
-        $data = $request->all();
-        $tipoTransaccion = TipoTransaccion::findOrFail($id);
-        $tipoTransaccion->fill($data);
-        $tipoTransaccion->save();
-
-        return response()->json($tipoTransaccion);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(int $id)
-    {
-        $tipoTransaccion = TipoTransaccion::findOrFail($id);
-        $tipoTransaccion->delete();
-
-        return response()->json([], 204);
-    }
+    return response()->json(['message' => 'Tipo transaccion delete successfully!!!'], 200);
+  }
 }
