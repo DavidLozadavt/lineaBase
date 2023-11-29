2de9fd9d5d0ebf4359b12a6bf5dce4ef481c5c6f
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-29 14:01:20 -0500

    fixed code of asignacionRolePermission

--- app/Http/Controllers/gestion_rol/AsignacionRolPermiso.php
+++ app/Http/Controllers/gestion_rol/AsignacionRolPermiso.php
@@ -11,45 +11,52 @@ use Spatie\Permission\Models\Role;
 class AsignacionRolPermiso extends Controller
 {
 
+  /**
+   * Get all permissions
+   *
+   * @return void
+   */
+  public function index()
+  {
+    $permisos = Permission::All();
 
-    public function index()
-    {
-        $permisos = Permission::All();
+    return response()->json($permisos);
+  }
 
-        return response()->json($permisos);
-    }
+  /**
+   * Assign permission by role
+   *
+   * @param Request $request
+   * @return void
+   */
+  public function permissionsByRole(Request  $request)
+  {
 
-    public function permissionsByRole(Request  $request)
-    {
+    $rol = $request->input('rol');
 
-        $rol = $request->input('rol');
-
-        $role = Role::findOrFail($rol);
-        $groupsWithRoles = $role->getPermissionNames();
-
-
-        return response()->json($groupsWithRoles);
-    }
-
-
-    public function assignFunctionality(Request $request)
-    {
-
-        // $user= User::find(auth()->user()->id);
-        // $user->assignRole("ADMINISTRADOR_VT");
-        // $user=Rol::all();
-
-        $roles = Role::find($request->idRol);
-        // dd($roles);
-        DB::table('role_has_permissions')
-            ->where('role_id', $request->idRol)
-            ->delete();
+    $role = Role::findOrFail($rol);
+    $groupsWithRoles = $role->getPermissionNames();
 
 
-        $roles->syncPermissions($request->input('funciones', []));
-        // $permisos= auth()->user();
-        // dd($request->all());
+    return response()->json($groupsWithRoles);
+  }
 
-        return $roles;
-    }
+  /**
+   * Assign functionality
+   *
+   * @param Request $request
+   * @return void
+   */
+  public function assignFunctionality(Request $request)
+  {
+
+    $roles = Role::find($request->idRol);
+
+    DB::table('role_has_permissions')
+      ->where('role_id', $request->idRol)
+      ->delete();
+
+    $roles->syncPermissions($request->input('funciones', []));
+    return $roles;
+  }
 }
