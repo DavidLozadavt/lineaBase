bcc9d44da2da08d0d2914503aa59b7008b1544bf
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-30 08:43:37 -0500

    Fixed error variable idPersona

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
+        'idPersona'
     ];
 
     /**
@@ -57,7 +58,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function persona()
     {
-        return $this->belongsTo(Persona::class, 'idpersona');
+        return $this->belongsTo(Persona::class, 'idPersona');
     }
 
     /**
